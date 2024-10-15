d08a888872f51500f01dc8906f83be8ab0f130cb
Fabian Topfstedt <topfstedt@schneevonmorgen.com>
2024-10-15 16:11:34 +0200

    format xfs partition gets set directly

--- pkg/remotecommand/command/formatxfs.go
+++ pkg/remotecommand/command/formatxfs.go
@@ -2,6 +2,7 @@ package command
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/fabiant7t/totalos/pkg/remotecommand"
 	"golang.org/x/crypto/ssh"
@@ -21,15 +22,20 @@ func FormatXFS(m remotecommand.Machine, device, diskUUID, partLabel, partUUID st
 	if partUUID == "" {
 		partUUID = "61291e61-291e-6129-1e61-291e61291e01"
 	}
+	part := fmt.Sprintf("%s1", device)
+	if strings.Contains(device, "nvme") {
+		part = fmt.Sprintf("%sp1", device)
+	}
 	cmd := fmt.Sprintf(`
     export DEVICE=%s \
+    export PARTITION=%s \
     && wipefs -af ${DEVICE} \
     && parted ${DEVICE} --script mklabel gpt \
     && sgdisk --disk-guid=%s ${DEVICE} \
     && parted ${DEVICE} --script mkpart primary xfs %s %s \
-    && mkfs.xfs -f -L %s -m uuid=%s ${DEVICE}*1
-  `, device, diskUUID, "0%", "100%", partLabel, partUUID)
-	if _, err := remotecommand.Command(m, cmd, cb); err != nil {
+    && mkfs.xfs -f -L %s -m uuid=%s ${PARTITION}
+  `, device, part, diskUUID, "0%", "100%", partLabel, partUUID)
+	if _, err := remotecommand.Command(m, cmd, cb); err == nil {
 		return fmt.Errorf("Remote command FormatXFS failed: %w", err)
 	}
 	return nil
